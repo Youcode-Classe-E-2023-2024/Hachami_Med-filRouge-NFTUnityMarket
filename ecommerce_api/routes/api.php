6ab0e2daa9d5561398762d52345f3e8b939da0ba
MedHachami <moha.hach.9@gmail.com>
2024-04-19 14:43:33 +0100

    profile endpoint

--- ecommerce_api/routes/api.php
+++ ecommerce_api/routes/api.php
@@ -18,8 +18,9 @@ Route::post("login", [ApiController::class, "login"]);
 
 Route::group(["middleware" => ["auth:api"]], function(){
 
-    Route::get("refresh", [ApiController::class, "refreshToken"]);
-Route::group(["middleware" => ["auth:api"]], function(){
 
+
+    Route::get("profile", [ApiController::class, "profile"]);
+    Route::get("refresh", [ApiController::class, "refreshToken"]);
     Route::get("logout", [ApiController::class, "logout"]);
 });
