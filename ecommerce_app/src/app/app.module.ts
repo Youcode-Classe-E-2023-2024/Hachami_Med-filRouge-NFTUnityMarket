ae78b9ee2ba55df854e516d10fbce378c2212cb4
MedHachami <moha.hach.9@gmail.com>
2024-04-26 11:32:30 +0100

    Update app.module.ts

--- ecommerce_app/src/app/app.module.ts
+++ ecommerce_app/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
+import { ShopComponent } from './modules/home/shop/shop.component';
  
 @NgModule({
   declarations: [
     AppComponent,
+    // ShopComponent,
   ],
   imports: [
     BrowserModule,
